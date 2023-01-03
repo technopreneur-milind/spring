daed5eff2720c90f81fe10a361635d21f617c841
Technopreneur Milind <milind.technopreneur@gmail.com>
2023-01-03 18:33:51 +0000

    add projects

---
+++ actuatordemo/src/main/java/com/technopreneur/spring/ActuatordemoApplication.java
@@ -0,0 +1,13 @@
+package com.technopreneur.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatordemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatordemoApplication.class, args);
+	}
+
+}
